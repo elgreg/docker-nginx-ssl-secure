83a73cc50e04df31ef92ac512cf63e10401554fb
Greg Lavallee <greg.lavallee@slate.com>
2018-08-09 21:36:22 -0400

    Remove my docker-for-mac thing

--- start-no-sw.sh
+++ start-no-sw.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# docker build --no-cache -telgreg/nginx-ssl:latest .
-
-# This is an example
-
-# I use this to generate my local mac's IP. Comment out if you'd like. Comes in handy
-export DOCKER_FOR_MAC_IP=`docker run alpine ping -n -q -c 1 docker.for.mac.localhost | grep -o -E  [0-9^/]+\.[0-9^/]+\.[0-9^/]+\.[0-9^/]+ | head -n 1 | xargs echo -n`;
+echo "Building without own certificate authority."
 
 docker run \
 -p 80:80 \
 -p 443:443 \
 -e 'DH_SIZE=512' \
 -e 'SSL_DOMAIN=local.dev.club.stuff' \
---add-host="mac.local:$DOCKER_FOR_MAC_IP" \
 -v `pwd`/etc/nginx/:/etc/nginx/external/ \
 -v `pwd`/etc/service-worker-example/:/usr/share/nginx/html/ \
 elgreg/nginx-ssl:latest
--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# docker build --no-cache -telgreg/nginx-ssl:latest .
-
-# This is an example
-
-# I use this to generate my local mac's IP. Comment out if you'd like. Comes in handy
-export DOCKER_FOR_MAC_IP=`docker run alpine ping -n -q -c 1 docker.for.mac.localhost | grep -o -E  [0-9^/]+\.[0-9^/]+\.[0-9^/]+\.[0-9^/]+ | head -n 1 | xargs echo -n`;
+echo "Building with certificate authority."
 
 docker run \
 -p 80:80 \
@@ -13,7 +8,6 @@ docker run \
 -e 'DH_SIZE=512' \
 -e 'USE_MINICA=1' \
 -e 'SSL_DOMAIN=local.dev.club.stuff' \
---add-host="mac.local:$DOCKER_FOR_MAC_IP" \
 -v `pwd`/etc/nginx/:/etc/nginx/external/ \
 -v `pwd`/etc/service-worker-example/:/usr/share/nginx/html/ \
 elgreg/docker-nginx-ssl-secure:latest
